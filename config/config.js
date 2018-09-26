7cd7af99ad319ba7c4887c57147fa6feb1760a3c
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-09-26 17:14:50 -0500

    Fixed config.js in config for deployment

--- config/config.js
+++ config/config.js
@@ -9,17 +9,14 @@ module.exports = {
     'dialect': 'mysql'
   },
   'test': {
-    'username': 'root',
-    'password': null,
+    'username': process.env.DB_USERNAME_DEV,
+    'password': process.env.DB_PASSWORD_DEV,
     'database': 'utilifleetDB_CI',
     'host': '127.0.0.1',
     'dialect': 'mysql'
   },
   'production': {
-    'username': 'root',
-    'password': null,
-    'database': 'utilifleetDB',
-    'host': '127.0.0.1',
+    'use_env_variable': 'JAWSDB_URL',
     'dialect': 'mysql'
   }
 };
