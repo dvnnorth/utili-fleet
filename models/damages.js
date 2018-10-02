b99684e76186b37906155c8cfbe6dcb010c83834
joens16 <joens16@hotmail.com>
2018-10-02 14:25:34 -0500

    models created -  clients, damages, users, drivers

---
+++ models/damages.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Damages = sequelize.define('Damages', {
+    vehicle: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    section: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    claim: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+  });
+  Damages.associate = function (models) {
+    // associations can be defined here
+  };
+  return Damages;
+};
