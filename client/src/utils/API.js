d2df86b85ea2a85e560b3f102e77a5a6246b6a73
joens16 <joens16@hotmail.com>
2018-10-12 21:04:55 -0500

    working on vehicles list, not done

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,13 @@
-/*
 import axios from 'axios';
 
 export default {
+  /*
   register: function (data) {
     return axios.post('/register', data).then(res => {
       console.log(res);
     });
   },
+
   login: function (data) {
     return axios.post('/signin', data).then(res => {
       console.log(res);
@@ -17,11 +18,71 @@ export default {
   logout: function () {
     return axios.get('/logout');
   },
+*/
 
+/////////////////////////// Vehicles /////////////////////////
+ 
 
-  getAllVehicles: function () {
-    return axios.get('/api/vehicles')
+getAllVehicles: () => {
+    return (axios.get("/api/vehicles"));
+  },
+getAllVehiclesByVIN: () => {
+    return (axios.get("/api/vehicle/:VIN"));
+  },
+postVehicles: (data) => {
+  const vehicleData = {
+    UnitNumber: data.UnitNumber,
+    VIN: data.VIN,
+    ModelYear: data.ModelYear,
+    Make: data.Make,
+    Model: data.Model,
+    Series: data.Series,
+    VehicleType: data.VehicleType,
+    BodyClass: data.BodyClass,
+    ExteriorColor: data.ExteriorColor,
+    InteriorColor: data.InteriorColor,
+    LicencePlate: data.LicencePlate,
+    Mileage: data.Mileage,
+    MaxMileage: data.MaxMileage,
+    NetCost: data.NetCost,
+    DepreciationStart: data.DepreciationStart,
+    DepreciationEnd: data.DepreciationEnd,
+    DepreciationRateYearly: DepreciationRateYearly,
+    TollTagSerial: data.TollTagSerial
+  };
+  return (axios.post("/api/vehicles"));
+  },
+
+updateVehicles: () => {
+  const vehicleData = {
+    UnitNumber: data.UnitNumber,
+    VIN: data.VIN,
+    ModelYear: data.ModelYear,
+    Make: data.Make,
+    Model: data.Model,
+    Series: data.Series,
+    VehicleType: data.VehicleType,
+    BodyClass: data.BodyClass,
+    ExteriorColor: data.ExteriorColor,
+    InteriorColor: data.InteriorColor,
+    LicencePlate: data.LicencePlate,
+    Mileage: data.Mileage,
+    MaxMileage: data.MaxMileage,
+    NetCost: data.NetCost,
+    DepreciationStart: data.DepreciationStart,
+    DepreciationEnd: data.DepreciationEnd,
+    DepreciationRateYearly: DepreciationRateYearly,
+    TollTagSerial: data.TollTagSerial
+  };
+    return (axios.put("/api/vehicle/:id"));
+  },
+
+deleteVehicles: () => {
+    return (axios.delete("/api/vehicle/:id"));
+    
   }
+
+/////////////////////////// Vehicles/////////////////////////
+  
 };
 
-*/
